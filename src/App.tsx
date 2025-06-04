a2154a2a766013953b6414455a4313d1c00ced67
youareagenius <youareagenius@example.com>
2025-06-04 20:52:37 +0900

    Update tap popup to +1

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const App: React.FC = () => {
   const [levelIndex, setLevelIndex] = useState(6);
   const [points, setPoints] = useState(22749365);
   const [clicks, setClicks] = useState<{ id: number, x: number, y: number }[]>([]);
-  const pointsToAdd = 11;
+  const pointsToAdd = 1;
   const profitPerHour = 126420;
 
   const [dailyCipherTimeLeft, setDailyCipherTimeLeft] = useState("");
